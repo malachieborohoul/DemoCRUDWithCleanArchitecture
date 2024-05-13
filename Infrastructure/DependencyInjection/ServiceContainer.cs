cb7ce0d1302a0c2db7a8cb3b8bc040c2a4a6975e
malachieborohoul <malachieborohoul@gmail.com>
2024-05-13 17:20:27 +0100

    Set Dependency Injection in Infrastructure folder

--- Infrastructure/DependencyInjection/ServiceContainer.cs
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -1,6 +1,52 @@
+using System.Text;
+using Domain.Entity.Authentication;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
 namespace Infrastructure.DependencyInjection;
 
-public class ServiceContainer
+public static class ServiceContainer
 {
-    
+   public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+   {
+      services.AddDbContext<AppDbContext>(o => o.UseSqlServer(config.GetConnectionString("ConnectionStrings")));
+      services.AddIdentityCore<ApplicationUser>().AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+         .AddSignInManager();
+      services.AddAuthentication(options =>
+      {
+         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(options =>
+
+         options.TokenValidationParameters = new TokenValidationParameters
+         {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidIssuer = config["Jwt:Issuer"],
+            ValidAudience = config["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!))
+         }
+      );
+      services.AddAuthentication();
+      services.AddAuthorization();
+      
+      //Add cors
+      services.AddCors(options =>
+      {
+         options.AddPolicy("WebUI",
+            builder => builder
+               .WithOrigins("https://localhost:7266")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials());
+      });
+      return services;
+   }
 }
