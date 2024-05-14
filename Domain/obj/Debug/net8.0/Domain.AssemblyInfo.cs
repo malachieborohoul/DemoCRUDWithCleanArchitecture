9c0f69cea2c57d8cd4a327a3365e5063bbeb3f7b
malachieborohoul <malachieborohoul@gmail.com>
2024-05-14 12:48:57 +0100

    Set AccountController

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6b7ae69d5ff950cd071ebdde99924597964010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c99482e46a987259c5e777fc98303787845dd02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
